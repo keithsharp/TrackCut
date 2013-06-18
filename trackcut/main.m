c47c47b5ce5d899b9b0d2655a67e99b28994e878
Keith Sharp <keith.sharp@gmail.com>
2013-06-18 10:57:45 +0100

    Initial Commit

---
+++ trackcut/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  trackcut
+//
+//  Created by Keith Sharp on 18/06/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
